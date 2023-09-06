55da7572ec3a2e3aef17eb43e0edb09cab846c9d
Gjokaj Luis (GJL) <luis.gjokaj@suva.ch>
2023-09-06 18:02:59 +0200

    factorial

--- faculty/main.c
+++ faculty/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+  int n = (int)&argv[1];
+  int res = 1;
+
+  for (int i = 1; i <= n; i++) {
+    res *= i; 
+  }
+  
+  printf("%d", res);
+
   return 0;
 }
